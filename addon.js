412739bdf98fba4dd30620af779ecbaa4eabdf44
xXCrash2BomberXx <91439080+xXCrash2BomberXx@users.noreply.github.com>
2026-01-10 16:25:12 -0600

    Update response headers for attachment filename

--- addon.js
+++ addon.js
@@ -131,8 +131,9 @@ app.get('/meta/:type/:id.json', async (req, res) => {
                                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36 Edg/143.0.0.0'
                                 },
                                 response: {
-                                    'Content-Disposition': 'attachment; filename="mono.ts"',
-                                    'Content-Type': 'video/mp2t'
+                                    'Content-Disposition': 'attachment; filename="mono.ts.m3u8"',
+                                    'Content-Type': 'video/mp2t',
+                                    'X-Content-Type-Options': 'none'
                                 }
                             }
                         }
