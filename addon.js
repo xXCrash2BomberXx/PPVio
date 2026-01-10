f20eaf28b3a9efcf8666a2965a6e73aa860748ae
xXCrash2BomberXx <91439080+xXCrash2BomberXx@users.noreply.github.com>
2026-01-09 19:58:02 -0600

    Add streaming endpoint for handling URL requests

--- addon.js
+++ addon.js
@@ -24,6 +24,21 @@ app.use((req, res, next) => {
     next();
 });
 
+app.get('/stream/:url', async (req, res, next) => {
+    const header = (await fetch(req.params.url, { method: 'HEAD' })).headers.get('content-type');
+    let content;
+    switch (header) {
+    case 'application/vnd.apple.mpegurl':
+    case 'application/x-mpegURL':
+        content = `#EXT-X-STREAM-INF:AVERAGE-BANDWIDTH=7440000,BANDWIDTH=9280000,RESOLUTION=1920x1080,FRAME-RATE=59.940,CODECS="avc1.4d402a,mp4a.40.2",CLOSED-CAPTIONS=NONE\n${req.params.url.replace('index.m3u8', 'tracks-v1a1/mono.ts.m3u8')}`;
+        break;
+    default:
+        throw new Error(`Unknown header type: "${header}"`)
+    }
+    res.set('Content-Type', header);
+    return res.send(content);
+});
+
 let streams;
 let lastFetchTime = 0;
 async function getData() {
@@ -112,7 +127,7 @@ app.get('/meta/:type/:id.json', async (req, res) => {
                     released: new Date(1000 * stream.starts_at).toISOString(),
                     thumbnail: stream.poster,
                     streams: [{
-                        url: atob((await (await fetch(stream.iframe)).text()).match(/(?<=atob\(").*?(?="\))/)?.[0]),
+                        url: `${req.protocol}://${req.get('host')}/stream/${atob((await (await fetch(stream.iframe)).text()).match(/(?<=atob\(").*?(?="\))/)?.[0])}`,
                         name: stream.uri_name,
                         behaviorHints: {
                             notWebReady: true,
